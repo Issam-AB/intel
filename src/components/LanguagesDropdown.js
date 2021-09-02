4ceb6994b7fa8264d8e4858d8fb9b1b5893f2def
ABOULFADL <issam.aboulfadl05@gmail.com>
2021-09-02 15:09:23 +0100

    first commit

---
+++ src/components/LanguagesDropdown.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { ChevronDown } from "react-feather";
+
+import {
+  Tooltip,
+  Menu,
+  MenuItem,
+  IconButton as MuiIconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Collapse,
+} from "@material-ui/core";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { makeStyles } from "@material-ui/core/styles";
+
+const IconButton = styled(MuiIconButton)`
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const Flag = styled.img`
+  border-radius: 50%;
+  width: 34px;
+  height: 34px;
+`;
+
+const useStyles = makeStyles((theme) => ({
+  menuappbar: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+    //borderBottom: "1px solid dark",
+  },
+}));
+
+function LanguagesDropdown() {
+  const classes = useStyles();
+  const [anchorMenu, setAnchorMenu] = React.useState(null);
+  const [open, setOpen] = React.useState(true);
+
+  const toggleMenu = (event) => {
+    setAnchorMenu(event.currentTarget);
+  };
+
+  const closeMenu = () => {
+    setAnchorMenu(null);
+  };
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <React.Fragment>
+      <Tooltip title="Account">
+        <IconButton
+          aria-owns={Boolean(anchorMenu) ? "menu-appbar" : undefined}
+          aria-haspopup="true"
+          onClick={toggleMenu}
+          color="default"
+        >
+          <Flag src="/static/img/avatars/avatar-1.jpg" alt="avatar" />
+          <ChevronDown />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        classes={classes.menuappbar}
+        anchorEl={anchorMenu}
+        keepMounted
+        open={Boolean(anchorMenu)}
+        onClose={closeMenu}
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <MenuItem onClick={closeMenu}>My Profile</MenuItem>
+        <ListItem button onClick={handleClick}>
+          <ListItemText primary="User" />
+          {open ? <ChevronRightIcon /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={!open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Billing" />
+            </ListItem>
+
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Integration" />
+            </ListItem>
+
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Support" />
+            </ListItem>
+
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Sign Out" />
+            </ListItem>
+          </List>
+        </Collapse>
+      </Menu>
+    </React.Fragment>
+  );
+}
+
+export default LanguagesDropdown;
---
+++ src/routes/index.js
@@ -0,0 +1,228 @@
+/* eslint-disable import/first */
+import React from "react";
+
+import async from "../components/Async";
+
+import {
+  Activity,
+  Bell,
+  CreditCard,
+  Grid,
+  Compass,
+  Filter,
+  FileText,
+  Slash,
+  Monitor,
+  PhoneIncoming,
+  Sliders,
+  Users,
+  Settings,
+  User,
+} from "react-feather";
+
+// All pages that rely on 3rd party components (other than Material-UI) are
+// loaded asynchronously, to keep the initial JS bundle to a minimum size
+
+// Guards
+import AuthGuard from "../components/AuthGuard";
+
+// Components components
+import Accordion from "../pages/components/Accordion";
+import Alerts from "../pages/components/Alerts";
+import Avatars from "../pages/components/Avatars";
+
+// Dashboards components
+// const Default = async(() => import("../pages/dashboards/Default"));
+const Analytics = async(() => import("../pages/dashboards/Analytics"));
+// Pages components
+import Blank from "../pages/pages/Blank";
+import InvoiceDetails from "../pages/pages/InvoiceDetails";
+import InvoiceList from "../pages/pages/InvoiceList";
+
+import Pricing from "../pages/pages/Pricing";
+import _Settings from "../pages/pages/Settings";
+import Chat from "../pages/pages/Chat";
+const Profile = async(() => import("../pages/pages/Profile"));
+const Tasks = async(() => import("../pages/pages/Tasks"));
+
+// Landing
+import Landing from "../pages/presentation/Landing";
+
+// Protected routes
+import ProtectedPage from "../pages/protected/ProtectedPage";
+
+const marketingPerformance = {
+  id: "Marketing Performance",
+  path: "/dashboard",
+  containsHome: true,
+  children: [
+    {
+      path: "/dashboard/analytics",
+      name: "Overview",
+      icon: <Compass />,
+      component: Analytics,
+    },
+  ],
+  component: null,
+};
+
+const websiteMetrics = {
+  id: "Website Metrics",
+  path: "/dashboard",
+  containsHome: true,
+  children: [
+    {
+      path: "/dashboard/analytics",
+      name: "Dashboard",
+      icon: <Activity />,
+      component: Analytics,
+    },
+  ],
+  component: null,
+};
+
+const websiteLead = {
+  id: "Website Leads (forms)",
+  path: "/pages",
+  children: [
+    {
+      path: "/pages/profile",
+      name: "Good Leads",
+      icon: <Filter />,
+      badge: "3",
+      component: Profile,
+    },
+    {
+      path: "/pages/settings",
+      name: "Blocked Leads",
+      icon: <Slash />,
+      component: _Settings,
+    },
+    {
+      path: "/pages/pricing",
+      name: "Custom Filters",
+      icon: <Sliders />,
+      component: Pricing,
+    },
+    {
+      path: "/pages/chat",
+      name: "Notifications",
+      badge: "3",
+      icon: <Bell />,
+      component: Chat,
+    },
+    {
+      path: "/pages/blank",
+      name: "Integrations",
+      icon: <Grid />,
+      component: Blank,
+    },
+    {
+      path: "/pages/blank",
+      name: "Settings",
+      icon: <Settings />,
+      component: Blank,
+    },
+  ],
+  component: null,
+};
+
+const callTracking = {
+  id: "Call Tracking",
+  path: "/invoices",
+  children: [
+    {
+      path: "/invoices",
+      name: "Call Log",
+      icon: <PhoneIncoming />,
+      component: InvoiceList,
+    },
+    {
+      path: "/invoices/detail",
+      name: "Reports",
+      icon: <FileText />,
+      component: InvoiceDetails,
+    },
+  ],
+  component: null,
+};
+
+const support = {
+  id: "Support",
+  path: "/tasks",
+  component: Tasks,
+  children: null,
+};
+
+const accountSettings = {
+  id: "Account Settings",
+  path: "/components",
+  children: [
+    {
+      path: "/components/alerts",
+      name: "My Profile",
+      icon: <User />,
+      component: Alerts,
+    },
+    {
+      path: "/components/accordion",
+      name: "Users",
+      icon: <Users />,
+      component: Accordion,
+    },
+    {
+      path: "/components/avatars",
+      name: "Biling",
+      icon: <CreditCard />,
+      component: Avatars,
+    },
+  ],
+  component: null,
+};
+
+const landingRoutes = {
+  id: "Landing Page",
+  path: "/",
+  header: "Docs",
+  icon: <Monitor />,
+  component: Landing,
+  children: null,
+};
+
+// This route is only visible while signed in
+const protectedPageRoutes = {
+  id: "Private",
+  path: "/private",
+  component: ProtectedPage,
+  children: null,
+  guard: AuthGuard,
+};
+
+// Routes using the Dashboard layout
+export const dashboardLayoutRoutes = [
+  marketingPerformance,
+  websiteMetrics,
+  websiteLead,
+  callTracking,
+  support,
+  accountSettings,
+];
+
+// // Routes using the Auth layout
+// export const authLayoutRoutes = [authRoutes];
+
+// Routes using the Presentation layout
+export const presentationLayoutRoutes = [landingRoutes];
+
+// Routes that are protected
+export const protectedRoutes = [protectedPageRoutes];
+
+// Routes visible in the sidebar
+export const sidebarRoutes = [
+  marketingPerformance,
+  websiteMetrics,
+  websiteLead,
+  callTracking,
+  support,
+  accountSettings,
+];
